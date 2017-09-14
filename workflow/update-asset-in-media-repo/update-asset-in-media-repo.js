905f5b6c97c1319bde62e833ef2cfd6dbfb3ee97
loicbarbou <loic@triskel-inc.com>
2017-09-14 09:46:07 -0400

    Updated lambda to add the proxy essence to the repo and update the BMContent with added resource

--- workflow/update-asset-in-media-repo/update-asset-in-media-repo.js
+++ workflow/update-asset-in-media-repo/update-asset-in-media-repo.js
@@ -10,6 +10,84 @@ const SERVICE_REGISTRY_URL = process.env.SERVICE_REGISTRY_URL;
 
 fims.setServiceRegistryServicesURL(SERVICE_REGISTRY_URL + "/Service");
 
+function AddEssencetoBMContent(bmc, essenceID) {
+    
+   
+    if (essenceID) {
+        // add the essence
+        if (bmc["ebucore:hasRelatedResource"] == undefined) {
+            bmc["ebucore:hasRelatedResource"] = [];
+        }
+        else {
+            if (Array.isArray(bmc["ebucore:hasRelatedResource"]) == false) {
+                if (bmc["ebucore:hasRelatedResource"].id == undefined) {
+                    bmc["ebucore:hasRelatedResource"] = [];
+                }
+                else {
+                    var tempValue = bmc["ebucore:hasRelatedResource"].id;
+                    bmc["ebucore:hasRelatedResource"] = [];
+                    bmc["ebucore:hasRelatedResource"].push({ "id": tempValue });
+                }
+            }
+
+        }
+
+        bmc["ebucore:hasRelatedResource"].push({ "@id": "" + essenceID + "" });
+        console.log("Adding essence to BMContent: " + JSON.stringify(bmc));
+    }
+
+    return bmc;
+}
+
+
+
+function getBMEssence(jsonObj) {
+    var context = jsonObj["@context"]
+    var graph = jsonObj["@graph"]
+    var bme = graph.find(function (g) { return g['@type'] == 'ebucore:BMEssence' });
+    if (bme === null || bme === undefined) {
+        console.error("No BMEssence found");
+    }
+    delete bme['@id'];
+    delete bme['ebucore:hasPart'];
+    bme["@type"] = "BMEssence";
+    bme["@context"] = context;
+    return bme;
+}
+
+function CreateBMEssenceShell(label, locator) {
+
+// Sample Essence Asset 
+// "@context": {
+//     "ebucore": "http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#",
+//     "esc": "http://www.eurovision.com#",
+//     "fims": "http://fims.tv#",
+//     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+//     "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+//     "xsd": "http://www.w3.org/2001/XMLSchema#"
+// },
+// "rdfs:label": "proxy",
+// "ebucore:locator": "https://s3.amazonaws.com/private-repo.ibc.fims.tv/transform-service-output/2001c256-d247-4d30-be49-02b2fe23d507.mp4",
+// "@type": "ebucore:BMEssence"
+
+
+
+    var bmeShell = {
+        "@context": {
+            "ebucore": "http://www.ebu.ch/metadata/ontologies/ebucore/ebucore#",
+            "esc": "http://www.eurovision.com#",
+            "fims": "http://fims.tv#",
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+            "xsd": "http://www.w3.org/2001/XMLSchema#"
+        },
+        "rdfs:label": label,
+        "ebucore:locator": locator,
+        "@type": "ebucore:BMEssence"
+    }
+    return bmeShell;
+}
+
 exports.handler = (event, context, callback) => {
 
     function nextStep(err, jsonEnvelop) {
@@ -38,5 +116,48 @@ exports.handler = (event, context, callback) => {
         console.error("No workflow_param found");
     }
 
-    nextStep(null, { payload, workflow_param });
+  var essenceId = null;
+  var updatedBmc = null;
+    async.waterfall([
+        function (callback) {
+            console.log("Retrieving Proxy Transcode job at " + workflow_param.transformjob_createproxy_id);
+            fims.httpGet(workflow_param.transformjob_createproxy_id, callback);
+        },
+        function (proxyJob, callback) {
+            console.log(JSON.stringify(proxyJob, null, 2));
+            return callback(null, proxyJob.jobOutput["ebucore:locator"]);
+        },
+        function (proxy_locator, callback) {
+            console.log("proxy locator for proxy = " +  proxy_locator ); 
+            bme = CreateBMEssenceShell("proxy",proxy_locator);
+            console.log("proxy essence to be created = " +  JSON.stringify(bme, null, 2) ); 
+            return fims.postResource("ebucore:BMEssence", bme, callback);
+        },
+        
+        function (bmEssence, callback) {
+            console.log("CreatedEssence:", JSON.stringify(bmEssence, null, 2));
+            essenceId = bmEssence.id; 
+            console.log("CreatedEssenceId:", essenceId);
+            callback();
+        },
+        function ( callback) {
+            console.log("Get Latest version of BMContent:", workflow_param.assetID);
+            fims.httpGet(workflow_param.assetID, callback);
+
+        }, function (bmc, callback) {
+
+            updatedBmc =  AddEssencetoBMContent (bmc, essenceId);
+           
+           return fims.httpPut (updatedBmc.id, updatedBmc, callback);
+
+            
+        },
+        function (bmContent, callback) {
+            console.log("updated BMContent:", JSON.stringify(bmContent, null, 2));
+            return callback();
+        }],
+    (err) => {
+        nextStep(err, {payload, workflow_param });
+    });
+
 }
